3b6fb472bee1b68dcc866d08ee5ed94158012462
hstandeffer <hcolestandeffer@gmail.com>
2020-07-06 20:54:17 -0500

    firebase example added

---
+++ src/components/firebase/context.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const FirebaseContext = React.createContext(null);
+
+export const withFirebase = Component => props => (
+    <FirebaseContext.Consumer>
+        {firebase => <Component {...props} firebase={firebase} />}
+    </FirebaseContext.Consumer>
+); //review how this works
+
+export default FirebaseContext;
+
+// function written out here to show how components are passed, 
+// const withFirebase = function(Component) {
+//     return function(props) {
+//         <FirebaseContext.Consumer>
+//              return function(firebase) {
+//                  <Component {...props} firebase={firebase} />
+//              }
+//         </FirebaseContext.Consumer>
+//     }
+// }
