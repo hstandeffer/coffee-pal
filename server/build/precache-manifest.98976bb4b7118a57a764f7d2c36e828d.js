d81871aae55bcc96cfb00129f8c50a91c0d7126e
hstandeffer <hcolestandeffer@gmail.com>
2021-01-24 14:37:42 -0600

    added build

---
+++ server/build/precache-manifest.98976bb4b7118a57a764f7d2c36e828d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89357a67a59c4269daf34dbde948e981",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57866c72a34fe69a46a7",
+    "url": "/static/css/main.e5549a26.chunk.css"
+  },
+  {
+    "revision": "3caf0c7b8eccdbd78524",
+    "url": "/static/js/2.73f08d41.chunk.js"
+  },
+  {
+    "revision": "8a2afccfde0b6f5774e03646896a8757",
+    "url": "/static/js/2.73f08d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57866c72a34fe69a46a7",
+    "url": "/static/js/main.5eaef2b5.chunk.js"
+  },
+  {
+    "revision": "8ef319677dbd373998ba",
+    "url": "/static/js/runtime-main.ae02cdbd.js"
+  },
+  {
+    "revision": "9aa3a744666365ef099a61bf3e06019c",
+    "url": "/static/media/bean-grey.9aa3a744.svg"
+  },
+  {
+    "revision": "c7866f453116575650efb044dd01c513",
+    "url": "/static/media/bean.c7866f45.svg"
+  }
+]);
