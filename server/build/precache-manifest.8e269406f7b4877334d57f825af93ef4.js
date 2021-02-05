b6d5926e9e6de83fc158eb828fb387a6d4f2239e
hstandeffer <hcolestandeffer@gmail.com>
2021-02-05 16:07:18 -0600

    deploying build to heroku

---
+++ server/build/precache-manifest.8e269406f7b4877334d57f825af93ef4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95600dfe3ebfd3780780923e7e8211f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0248437c68b52fca5b75",
+    "url": "/static/css/main.e5549a26.chunk.css"
+  },
+  {
+    "revision": "03182507921e4ec10def",
+    "url": "/static/js/2.57fabfda.chunk.js"
+  },
+  {
+    "revision": "a5b4b1ece5e1f4f4c0af3cfaf71c0f98",
+    "url": "/static/js/2.57fabfda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0248437c68b52fca5b75",
+    "url": "/static/js/main.5539e581.chunk.js"
+  },
+  {
+    "revision": "8ef319677dbd373998ba",
+    "url": "/static/js/runtime-main.ae02cdbd.js"
+  },
+  {
+    "revision": "9aa3a744666365ef099a61bf3e06019c",
+    "url": "/static/media/bean-grey.9aa3a744.svg"
+  },
+  {
+    "revision": "c7866f453116575650efb044dd01c513",
+    "url": "/static/media/bean.c7866f45.svg"
+  }
+]);
