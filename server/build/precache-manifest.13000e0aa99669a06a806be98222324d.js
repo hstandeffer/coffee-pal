6e4b412c95f826f227c5cb8b37347f4b9c9c698a
hstandeffer <hcolestandeffer@gmail.com>
2021-03-06 11:20:02 -0600

    suggestions build

---
+++ server/build/precache-manifest.13000e0aa99669a06a806be98222324d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe232e131e30bc98f7ec39920fd26f6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b60dc26778043294da4",
+    "url": "/static/css/main.e5549a26.chunk.css"
+  },
+  {
+    "revision": "66d16fa2817e17dad980",
+    "url": "/static/js/2.6b97ccca.chunk.js"
+  },
+  {
+    "revision": "a5b4b1ece5e1f4f4c0af3cfaf71c0f98",
+    "url": "/static/js/2.6b97ccca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b60dc26778043294da4",
+    "url": "/static/js/main.e7daeb08.chunk.js"
+  },
+  {
+    "revision": "8ef319677dbd373998ba",
+    "url": "/static/js/runtime-main.ae02cdbd.js"
+  },
+  {
+    "revision": "9aa3a744666365ef099a61bf3e06019c",
+    "url": "/static/media/bean-grey.9aa3a744.svg"
+  },
+  {
+    "revision": "c7866f453116575650efb044dd01c513",
+    "url": "/static/media/bean.c7866f45.svg"
+  },
+  {
+    "revision": "141c706b93df7eab749fb71ea2f1dac3",
+    "url": "/static/media/check.141c706b.svg"
+  },
+  {
+    "revision": "0d3ac92f340f63dd2c126312b35017c7",
+    "url": "/static/media/globe.0d3ac92f.svg"
+  },
+  {
+    "revision": "b21d7670fba32070d00a720de8d5f4f3",
+    "url": "/static/media/heat.b21d7670.svg"
+  }
+]);
